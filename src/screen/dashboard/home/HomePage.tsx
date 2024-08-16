57e03f6de1dfa43af313618ffbfbd23ea544a016
Noel Osarinmwian <osarinmwian@bepeerless.co>
2024-08-16 11:09:02 +0100

    fix

--- src/screen/dashboard/home/HomePage.tsx
+++ src/screen/dashboard/home/HomePage.tsx
@@ -1,15 +1,57 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, { useState } from 'react';
+import {
+  Alert,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import axios from 'axios';
 
 const HomePage: React.FC = () => {
-  const post = () => {
-    // const response =  axois.
+  const [idData, setIddata] = useState<string | undefined>(undefined);
+
+  const post = async () => {
+    try {
+      if (!idData) {
+        Alert.alert('ID data not found')
+        return;
+      }
+
+      const response = await axios.post(`https://jsonplaceholder.typicode.com/posts/${idData}`);
+      console.log('Post successful', response.data);
+    } catch (error) {
+      console.error('Error posting data', error);
+      Alert.alert('Error posting data')
+    }
   };
+
   return (
     <SafeAreaView>
       <Text>LANDING</Text>
+      <TextInput
+        value={idData}
+        onChangeText={(val: string) => setIddata(val)}
+        placeholder="Enter ID"
+      />
+      <TouchableOpacity onPress={post} style={styles.button}>
+        <Text style={styles.buttonText}>CLICK</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
 
 export default HomePage;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
--- src/screen/dashboard/profilePage/DetailPage.tsx
+++ src/screen/dashboard/profilePage/DetailPage.tsx
@@ -1,16 +1,38 @@
-import React, {useEffect} from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import {FlatList, SafeAreaView, Text, View} from 'react-native';
 
 const DetailPage: React.FC = () => {
-  const fetchPost = () => {
-    // const response =  axios.
+  const [fetchData, setFetchData] = useState();
+  const fetchPost = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts',
+      );
+
+      const data = response.data;
+      setFetchData(data);
+      return response;
+    } catch (error) {
+      return error;
+    }
   };
   useEffect(() => {
     fetchPost();
   }, []);
   return (
     <SafeAreaView>
-      <Text>PROFILE</Text>
+      <Text>DETAILS</Text>
+      <FlatList
+        data={fetchData}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => (
+          <View>
+            <Text>{item.title}</Text>
+            <Text>{item.body}</Text>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 };
