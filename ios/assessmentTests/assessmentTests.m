29c026213430e71d9870d8403db3d1dfcee69378
Noel Osarinmwian <osarinmwian@bepeerless.co>
2024-08-16 11:11:41 +0100

    fix

--- ios/assessmentTests/assessmentTests.m
+++ ios/assessmentTests/assessmentTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RiderAppTests : XCTestCase
+@interface assessmentTests : XCTestCase
 
 @end
 
-@implementation RiderAppTests
+@implementation assessmentTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
